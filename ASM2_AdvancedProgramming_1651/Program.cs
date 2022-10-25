bb78255cf89300ebd8d1bf2126293e5a55ea2dce
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2022-10-26 06:33:37 +0700

    add menu functions

--- ASM2_AdvancedProgramming_1651/Program.cs
+++ ASM2_AdvancedProgramming_1651/Program.cs
@@ -3,6 +3,8 @@
 Main();
 void Main()
 {
+    ShowRoom showRoom = new ShowRoom();
+
     int choice;
     while (true)
     {
@@ -17,5 +19,69 @@ void Main()
         Console.WriteLine("|__________________________________|");
         Console.Write("=> Enter your choice: ");
         choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                while (true)
+                {
+                    Console.WriteLine(" c: To add normal car");
+                    Console.WriteLine(" s: To add super car");
+                    Console.WriteLine(" v: To exit menu");
+                    Console.WriteLine("=> Enter your option: ");
+                    string option = Console.ReadLine();
+                    if (option.Equals("c"))
+                    {
+                        Console.Write("=> Enter Id: ");
+                        int idCar = int.Parse(Console.ReadLine());
+                        Console.Write("=> Enter Brand: ");
+                        string brand = Console.ReadLine();
+                        Console.Write("=> Enter Model: ");
+                        string modeli = Console.ReadLine();
+                        Console.Write("=> Enter price: ");
+                        double price = double.Parse(Console.ReadLine());
+                        showRoom.AddCar(new Car(idCar, brand, modeli, price));
+                    }
+                    else if (option.Equals("s"))
+                    {
+                        Console.Write("=> Enter Id: ");
+                        int idCar = int.Parse(Console.ReadLine());
+                        Console.Write("=> Enter Brand: ");
+                        string brand = Console.ReadLine();
+                        Console.Write("=> Enter Model: ");
+                        string _modeli = Console.ReadLine();
+                        Console.Write("=> Enter price: ");
+                        double price = double.Parse(Console.ReadLine());
+                        showRoom.AddCar(new SuperCar(idCar, brand, _modeli, price));
+                    }
+                    else if (option.Equals("v"))
+                    {
+                        break;
+                    }
+                }
+                break;
+            case 2:
+                Console.WriteLine(showRoom.PrintInfor());
+                break;
+            case 3:
+                string branch = Console.ReadLine();
+                Console.WriteLine(showRoom.FindCarsByBrand(branch));
+                break;
+            case 4:
+                string _model = Console.ReadLine();
+                Console.WriteLine(showRoom.FindCarsByModel(_model));
+                break;
+            case 5:
+                string model = Console.ReadLine();
+                showRoom.RemoveCarByModel(model);
+                break;
+            case 6:
+                
+            case 0:
+                return;
+            default:
+                Console.WriteLine("! Please should input correct option !");
+                break;
+        }
     }
 }
