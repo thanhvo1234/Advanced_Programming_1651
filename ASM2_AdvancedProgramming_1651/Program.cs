60bc525fea2ee4a7d4f5479c9b77b1af3b23d52d
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2022-10-26 06:30:11 +0700

    create menu in Program class(main)

--- ASM2_AdvancedProgramming_1651/Program.cs
+++ ASM2_AdvancedProgramming_1651/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ASM2_AdvancedProgramming_1651;
+
+Main();
+void Main()
+{
+    int choice;
+    while (true)
+    {
+        Console.WriteLine(" _________ CAR MANAGEMENT _________");
+        Console.WriteLine("| 1: Add new car                   |");
+        Console.WriteLine("| 2: Show information of all cars  |");
+        Console.WriteLine("| 3: Find all cars by branch       |");
+        Console.WriteLine("| 4: Find car by model             |");
+        Console.WriteLine("| 5: Remove car by model           |");
+        Console.WriteLine("| 6: Update car by Id              |");
+        Console.WriteLine("| 0: Exit                          |");
+        Console.WriteLine("|__________________________________|");
+        Console.Write("=> Enter your choice: ");
+        choice = int.Parse(Console.ReadLine());
+    }
+}
