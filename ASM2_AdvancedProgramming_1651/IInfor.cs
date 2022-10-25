aaa247d2f65ac482324ad0c6d831075900e05653
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2022-10-26 06:07:31 +0700

    interface IInfor

---
+++ ASM2_AdvancedProgramming_1651/IInfor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASM2_AdvancedProgramming_1651;
+
+namespace ASM2_AdvancedProgramming_1651
+{
+    public interface IInfor
+    {
+        string PrintInfor();
+    }
+}
