4fe9cf5006f9d4d9e75e8ac220fc379be172ecb8
thanhvo1234 <thanhvnkbgcd201808@fpt.edu.vn>
2022-10-26 06:21:26 +0700

    SuperCar Class

--- ASM2_AdvancedProgramming_1651/Car.cs
+++ ASM2_AdvancedProgramming_1651/Car.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ASM2_AdvancedProgramming_1651;
 
 namespace ASM2_AdvancedProgramming_1651
 {
---
+++ ASM2_AdvancedProgramming_1651/SuperCar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ASM2_AdvancedProgramming_1651;
+
+namespace ASM2_AdvancedProgramming_1651
+{
+    internal class SuperCar : Car
+    {
+        public SuperCar(int id, string brand, string _model, double price) : base(id, brand, _model, price)
+        {
+        }
+
+        public override double Price
+        {
+            get
+            {
+                return base.Price * 1.3;
+            }
+            set
+            {
+                base.Price = value;
+            }
+        }
+    }
+}
